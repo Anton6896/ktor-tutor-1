b8e55eb3d6188c5ef89817e2350f1931dde87de2
anton-r <anton-r@live.com>
2022-03-05 19:05:03 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ktor-tutor-1
+
+learning Ktor from youtube 
+* https://www.youtube.com/watch?v=dsdrA0X7z_I&list=PLFmuMD2V4CkyR0Pa42Cqu5mIhH17uG8nN&index=2&ab_channel=ByteSizedScreencasts
---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,32 @@
+package com.example
+
+import io.ktor.application.*
+import com.example.plugins.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+//fun main(args: Array<String>): Unit =
+//    io.ktor.server.netty.EngineMain.main(args)
+//
+//@Suppress("unused")
+//fun Application.module() {
+//    configureRouting()
+//}
+
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        install(Routing){
+            homeRoute()
+        }
+    }.start()
+}
+
+
+fun Routing.homeRoute() {
+    get("/") {
+        call.respond("Hello there ")
+    }
+}
