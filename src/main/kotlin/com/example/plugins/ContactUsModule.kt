d381d311dd32e98db1e28449830780732135bbd6
anton-r <anton-r@live.com>
2022-03-05 19:26:01 +0200

    first

--- src/main/kotlin/com/example/plugins/ContactUsModule.kt
+++ src/main/kotlin/com/example/plugins/ContactUsModule.kt
@@ -1,13 +1,21 @@
 package com.example.plugins
 
 import io.ktor.application.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.contactUsModule() {
     routing {
-        get("/contact_us"){
+        get("/contact_us") {
+            println("uri : ${call.request.uri}")
+            println("headers: ${call.request.headers.names()}")
+            println("User-Agent : ${call.request.headers["User-Agent"]}")
+            println("query params : ${call.request.queryParameters.names()}")
+
             call.respond("contact us mesage")
         }
+
+
     }
 }
